baf1ff2ef6e260fd26050d8a91f4a19e620c4d55
Ali Sina <ali.sina1447@gmail.com>
2024-10-25 02:09:40 -0400

    ADD - initial spring boot backend project

---
+++ spring-backend/src/main/java/com/alisina/spacerent/SpacerentApplication.java
@@ -0,0 +1,13 @@
+package com.alisina.spacerent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacerentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacerentApplication.class, args);
+	}
+
+}
---
+++ spring-backend/src/test/java/com/alisina/spacerent/SpacerentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisina.spacerent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacerentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
